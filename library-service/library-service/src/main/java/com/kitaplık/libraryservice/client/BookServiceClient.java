3819a0d85cf08ac761356799fa3d0fcdb1a6dbe2
GorkemBedel <117198668+GorkemBedel@users.noreply.github.com>
2024-08-27 16:44:22 +0300

    Second Commit - Gateway added

--- library-service/library-service/src/main/java/com/kitaplık/libraryservice/client/BookServiceClient.java
+++ library-service/library-service/src/main/java/com/kitaplık/libraryservice/client/BookServiceClient.java
@@ -2,6 +2,9 @@ package com.kitaplık.libraryservice.client;
 
 import com.kitaplık.libraryservice.dto.BookDto;
 import com.kitaplık.libraryservice.dto.BookIdDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +15,23 @@ import javax.validation.constraints.NotEmpty;
 @FeignClient(name = "book-service", path = "/v1/book")
 public interface BookServiceClient {
 
+    Logger logger = LoggerFactory.getLogger(BookServiceClient.class);
+
     @GetMapping("/isbn/{isbn}")
+    @CircuitBreaker(name = "getBookByIsbnCircuitBreaker", fallbackMethod = "getBookFallback")
     ResponseEntity<BookIdDto> getBookByIsbn(@PathVariable String isbn);
 
+    default ResponseEntity<BookIdDto> getBookFallback(String isbn, Exception e) {
+        logger.info("Book not found with ISBN: " + isbn + "returning default BookDto object");
+        return ResponseEntity.ok(new BookIdDto("default-bookId", isbn));
+    }
+
     @GetMapping("/book/{id}")
+    @CircuitBreaker(name = "getBookByIdCircuitBreaker", fallbackMethod = "getBookByIdFallback")
     ResponseEntity<BookDto> getBookById(@PathVariable String id);
+
+    default ResponseEntity<BookDto> getBookByIdFallback(String id, Exception exception) {
+        logger.info("Book not found by id " + id + ", returning default BookDto object");
+        return ResponseEntity.ok(new BookDto(new BookIdDto("default-book", "isbn")));
+    }
 }
---
+++ library-service/library-service/src/main/java/com/kitaplık/libraryservice/exception/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package com.kitaplık.libraryservice.exception;
+
+public record ExceptionMessage(
+
+        String timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
+}
