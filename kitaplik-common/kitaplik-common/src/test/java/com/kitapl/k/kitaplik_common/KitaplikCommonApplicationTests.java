8b34db79397be4a19426692cf535bfe0acf9eca2
GorkemBedel <117198668+GorkemBedel@users.noreply.github.com>
2024-09-19 21:22:04 +0300

    vault added

---
+++ kitaplik-common/kitaplik-common/src/test/java/com/kitapl/k/kitaplik_common/KitaplikCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitapl.k.kitaplik_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitaplikCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- library-service/library-service/src/main/java/com/kitaplık/libraryservice/controller/LibraryController.java
+++ library-service/library-service/src/main/java/com/kitaplık/libraryservice/controller/LibraryController.java
@@ -5,12 +5,15 @@ import com.kitaplık.libraryservice.dto.LibraryDto;
 import com.kitaplık.libraryservice.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/v1/library")
 public class LibraryController {
@@ -18,6 +21,8 @@ public class LibraryController {
     Logger logger = LoggerFactory.getLogger(LibraryController.class);
     private final LibraryService libraryService;
     private final Environment environment;
+    @Value("${library-service.book.count}")
+    private Integer count;
 
 
     public LibraryController(LibraryService libraryService, Environment environment) {
@@ -28,6 +33,11 @@ public class LibraryController {
     @GetMapping("{id}")
     public ResponseEntity<LibraryDto> getLibraryById(@PathVariable String id){
         return ResponseEntity.ok(libraryService.getAllBooksInLibraryById(id));
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        return ResponseEntity.ok("Library service count : " + count);
     }
 
     @PostMapping
